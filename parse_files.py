d0197df4a67ac5e4757c2caa6fc1acdeb86746a2
Prasanna Pandey <tattwa224@gmail.com>
2024-12-10 23:32:12 +1300

    first one.

--- parse_files.py
+++ parse_files.py
@@ -6,6 +6,14 @@ data_folder = 'data'
 specs_folder = 'specs'
 output_folder = 'output'
 
+#Dict for mapping spec datatypes to pandas datatypes
+type_mapping = {
+    'TEXT': 'object',
+    'BOOLEAN': 'bool',
+    'INTEGER': 'int64',
+    'FLOAT': 'float64',
+}
+
 for data_file in os.listdir(data_folder):
     if data_file.endswith('.txt'):
         try:
@@ -17,11 +25,8 @@ for data_file in os.listdir(data_folder):
                 print(f"Error: Specification file {spec_file} not found for {data_file}. Skipping.")
                 continue
 
-            # Read the spec file
             try:
                 spec_df = pd.read_csv(spec_file, sep=',')
-                print(f"Specification for {file_prefix}:")
-                print(spec_df)
             except Exception as e:
                 print(f"Error reading specification file {spec_file}: {e}")
                 continue
@@ -30,27 +35,25 @@ for data_file in os.listdir(data_folder):
             column_widths = spec_df['width'].tolist()
             column_dtypes = spec_df['datatype'].tolist()
 
-            print(column_dtypes)
+            dtypes = {col: type_mapping[dt] for col, dt in zip(column_names, column_dtypes)}
 
             data_file_path = os.path.join(data_folder, data_file)
             try:
-                data_df = pd.read_fwf(data_file_path, widths=column_widths, names=column_names)
-                print(f"Data for {data_file}:")
-                print(data_df.dtypes)
+                data_df = pd.read_fwf(data_file_path, widths=column_widths, names=column_names, dtype=dtypes)
             except Exception as e:
                 print(f"Error reading data file {data_file_path}: {e}")
                 continue
 
-            output_file_path = os.path.join(output_folder, data_file.split('.')[0]) + '.ndjson' #TODO:- check!!!
-
             if not os.path.exists(output_folder):
                 os.makedirs(output_folder)
-            # Convert DataFrame to list of dictionaries
-            ndjson_lines = data_df.to_dict(orient='records')
 
+            output_file_path = os.path.join(output_folder, data_file.split('.')[0]) + '.ndjson'
+
+            ndjson_lines = data_df.to_dict(orient='records')
             with open(output_file_path, 'w') as f:
                 for line in ndjson_lines:
                     f.write(json.dumps(line) + '\n')
+            print(f"Output file written for {data_file.split('.')[0]}.")
 
         except Exception as e:
             print(f'ERROR: while processing {data_file}: {e}')
