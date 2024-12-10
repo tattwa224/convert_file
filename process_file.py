94b0e95e3b1c3362d2da0a0968b593ceb1834266
Prasanna Pandey <tattwa224@gmail.com>
2024-12-10 15:36:22 +1300

    first one.

---
+++ process_file.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+#open a data file, find corresponding spec file. using substring split by _
+#read contents of spec csv and pass data of data file to the method
+#generate new file with ndjson format
